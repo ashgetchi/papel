8e276fc0b43e30ed86c0e26b4aba60312307ddf0
Ash Atighetchi <ashgetchi@gmail.com>
2020-09-13 15:43:40 +0100

    finished stationery index building now working on designers

--- frontend/src/components/common/Stationery.js
+++ frontend/src/components/common/Stationery.js
@@ -21,14 +21,45 @@ class Stationery extends React.Component {
     
     return (
       <>
-        <h1>hello</h1>
-        <div className="three-col">
+        <div className="three-col content-font">
           {this.state.stationery.map(stat => {
             return (
-              <div className="three-col-content" key={stat._id}>
-                <h2>{stat.name}</h2>
-                <img src={stat.image}/>
+            // <div className="three-col-content" key={stat.id}>
+            //   <h2>{stat.name}</h2>
+            //   <img src={stat.image}/>
+            // </div>
+
+              <div className="column is-one-third-desktop is-one-third-tablet is-fullwidth has-text-centered" key={stat.id}>
+                {/* <Link to={`/gigs/${_id}`}> */}
+
+                <div className="gig-card">
+                  <div className="card-header content-box">
+                    <h2 className="has-text-centered gig-card">{stat.name}</h2>
+                  </div>
+
+                  <div className="card-image">
+                    <figure className="image image is-1by1">
+                    
+                      <img src={stat.image} alt={stat.name} loading="lazy" width="300" height="220" />
+                    </figure>
+                  </div>
+
+                  <div className="card-header has-text-centered content-box">
+                    <div className="card-content has-text-centered">
+                      <h5 className="">{stat.type}</h5>
+                      <h5 className="">{stat.designer}</h5>
+                      <h5 className="">{stat.quantity}</h5>
+                      <h5 className="">£{stat.price}</h5>
+                    </div>
+                  </div>
+
+
+                </div>
+
+                {/* </Link> */}
+
               </div>
+
             )
           })}
         </div>
